1dfdb4e93f44faf99ba9162cfd4ea9950a3968d3
Tushar Mittal <tushar.mittal01@nagarro.com>
2025-03-26 11:50:16 +0530

    Enhance layout and styling: add app-section class, update carousel component, and improve header content

--- src/app/header/carousel/carousel.component.ts
+++ src/app/header/carousel/carousel.component.ts
@@ -9,7 +9,14 @@ import { CarouselModule } from 'primeng/carousel';
   styleUrl: './carousel.component.scss',
 })
 export class CarouselComponent {
-  products = [];
+  products = [
+    {
+      url: 'https://balanceayurveda.in/cdn/shop/files/Foreground_1.png?v=1714022996',
+    },
+    {
+      url: 'https://balanceayurveda.in/cdn/shop/files/apple_cider_png.png?v=1729667986',
+    },
+  ];
 
   responsiveOptions = [
     {
